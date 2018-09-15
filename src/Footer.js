0330d521f76db56ed2910eb9d7a4e3b565bf6b60
Ender İMEN <enderimen@hotmail.com>
2018-09-16 00:03:51 +0300

    "Footer" component has been created.

---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export class Footer extends React.Component {
+
+    render(){
+
+        return(
+            <div className="footer">
+                <div class="sum">0 items</div>
+                <div class="all">All</div>
+                <div class="active">Active</div>
+                <div class="completed">Component</div>
+            </div>
+        );
+    }
+}
