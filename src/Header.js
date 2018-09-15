cc6d1d31d502dfe52902e47110796b527d737e0d
Ender İMEN <enderimen@hotmail.com>
2018-09-16 01:17:29 +0300

    Adding quests to quests is complete.

--- src/Header.js
+++ src/Header.js
@@ -2,14 +2,25 @@ import React from 'react';
 
 export class Header extends React.Component {
 
-    render(){
+    addTask(e) {
+        e.preventDefault();
+        const todo = document.getElementById("todo-add");
+        const todoAdd = todo.value; 
+        console.log("Çalıştı" + todoAdd);
+        todo.value = '';
+        this.props.addTask(todoAdd);
+    }
 
+    render(){
+    
         return(
             <div>
                 <h3 className="center">TODO List</h3>
                 <div className="header">
-                    <input type="text" className="todo-add-input" placeholder="What needs to be done?" />
-                    <button className="add-button">+</button>
+                    <form onSubmit={ this.addTask }>
+                        <input type="text" id="todo-add" className="todo-add-input" placeholder="What needs to be done?" />
+                        <button className="add-button" onClick={ this.addTask.bind(this) }>+</button>
+                    </form>
                 </div>
             </div>
         );
