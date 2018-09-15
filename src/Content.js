4e15be593e2e06d0d62dbb6c0e87df9640891f5e
Ender İMEN <enderimen@hotmail.com>
2018-09-16 00:03:40 +0300

    "Content" component has been created.

---
+++ src/Content.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export class Content extends React.Component {
+
+    render(){
+
+        return(
+                <div className="content">
+                    <ul type="none">
+                        <li>
+                            deneme
+                            <button className="isCompleted"></button>
+                        </li>
+                        <li>
+                            deneme
+                            <button className="isCompleted"></button>
+                        </li>
+                        <li>
+                            deneme
+                            <button className="isCompleted"></button>
+                        </li>
+                        <li>
+                            deneme
+                            <button className="isCompleted"></button>
+                        </li>
+                        <li>
+                            deneme
+                            <button className="isCompleted"></button>
+                        </li>
+                    </ul>
+                </div>
+        );
+    }
+}
