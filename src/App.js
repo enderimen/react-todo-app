5bbd4593b95515717e834cde28427f341d8ada27
Ender İMEN <enderimen@hotmail.com>
2018-09-16 00:04:10 +0300

    "App" root component has been created.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import {TodoList} from './TodoList';
+import {Header} from './Header';
+import {Content} from './Content';
+import {Footer} from './Footer';
 
 class App extends Component {
   render() {
     return (
       
       <div>
-        Hello World!
+        <Header></Header>
+        <Content></Content>
+        <Footer></Footer>
+        <TodoList></TodoList>
       </div>
     );
   }
