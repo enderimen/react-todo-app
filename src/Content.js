ca6a5cf12e31febc25b69b87a96654416f8c642c
Ender İMEN <enderimen@hotmail.com>
2018-09-16 04:07:54 +0300

    "Task Remove" and "Task Done" completed!

--- src/Content.js
+++ src/Content.js
@@ -2,23 +2,44 @@ import React from 'react';
 
 export class Content extends React.Component {
 
-    render(){
+    constructor(){
+        super();
+    }
 
-        const items = this.props.tasks.map((element, i) => {
-            return(
-                <li key={ i }>
-                    { i +1 } { element }
-                    <button className="isCompleted"></button>
+    doneTask = (e) => {
+        
+        this.props.doneTask(e.target.parentNode.id);
+        
+    };
+
+    removeTask = (e)=> {
+    
+        this.props.removeTask(e.target.parentNode.id);
+
+    };
+
+    render(){
+            let items_sum = 0;
+            const items = this.props.tasks.map((element, i) => {
+                if(element.status === "passive" ){
+                    items_sum++;
+                }
+                let task_id = 'task_'+ i;
+                return(
+                <li key={ i } id= { task_id }>
+                    <span className="task" className={ element.status }>{ i + 1 } { element.text }</span>
+                    <span className="isCompleted" onClick={ this.doneTask }></span>
+                    <span className="remove" onClick={ this.removeTask }>X</span>
                 </li>
             );
         });
          
         return(
-                <div className="content">
-                    <ul type="none">
-                        { items }
-                    </ul>
-                </div>
+            <div className="content">
+                <ul type="none">
+                    { items }
+                </ul>
+            </div>
         );
     }
 }
