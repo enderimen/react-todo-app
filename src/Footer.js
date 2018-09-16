7ebb086ab3d70140ee0d332c0d4006a2f5105572
Ender İMEN <enderimen@hotmail.com>
2018-09-16 04:08:29 +0300

    some class name changed.

--- src/Footer.js
+++ src/Footer.js
@@ -8,7 +8,7 @@ export class Footer extends React.Component {
             <div className="footer">
                 <div className="sum">0 items</div>
                 <div className="all"><button className="all-btn">All</button></div>
-                <div className="active"><button className="all-btn">Active</button></div>
+                <div className="actives"><button className="all-btn">Active</button></div>
                 <div className="completed"><button className="completed-btn">Completed</button></div>
             </div>
         );
