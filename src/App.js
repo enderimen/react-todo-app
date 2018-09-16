4efef814e4e4276a93f4345a6d19948eb30a6047
Ender İMEN <enderimen@hotmail.com>
2018-09-16 04:08:06 +0300

    ...

--- src/App.js
+++ src/App.js
@@ -9,27 +9,59 @@ class App extends Component {
   constructor(){
     super();
     this.state = { tasks : [
-      "Kitap Oku",
-      "Randevu Al"
+      {text: "Kitap Oku", status: "passive"},
+      {text: "Randevu Al", status: "passive"}
       ]};
-      this.addTask = this.addTask.bind(this);
+      this.addTask    = this.addTask.bind(this);
+      this.doneTask   = this.doneTask.bind(this);
+      this.removeTask = this.removeTask.bind(this);
   }
   
   addTask(val){
     let updateList = this.state.tasks;
-    updateList.push(val);
+    updateList.push({text: val, status:"passive"});
     this.setState({ tasks: updateList });
   }
 
-  render() {
+  doneTask(task_id){
+    task_id = task_id.replace("task_","");
 
+    let updateList = this.state.tasks;
+
+    let newStatus = "active";
+
+    let currentStatus = updateList[task_id].status;
+
+    if(currentStatus === "active"){
+      newStatus = "passive";
+    }
+
+    console.log(task_id);
+    
+    updateList[task_id].status = newStatus;
+    this.setState({ tasks: updateList });
+  }
+
+  removeTask(task_id){
+    task_id = task_id.replace("task_","");
+
+    let updateList = this.state.tasks;
+    
+    updateList.splice(task_id, 1);
+
+    this.setState({ tasks: updateList });
+
+  }
+
+  render() {
+    
     return (
       
       <div>
         <Header addTask= { this.addTask }></Header>
-        <Content tasks = { this.state.tasks }></Content>
-        <Footer></Footer>
-        <TodoList></TodoList>
+        <Content tasks = { this.state.tasks } doneTask = { this.doneTask } removeTask = { this.removeTask }></Content>
+        <Footer />
+        <TodoList />
       </div>
     );
   }
