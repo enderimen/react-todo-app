0da0211a5eac2f0f69bd11ad47c99d3b9dd6c2ef
Ender İMEN <enderimen@hotmail.com>
2018-09-17 14:47:18 +0300

    listing by menus.

--- src/Content.js
+++ src/Content.js
@@ -2,8 +2,20 @@ import React from 'react';
 
 export class Content extends React.Component {
 
-    constructor(){
-        super();
+    constructor() {
+        super();     
+        this.state = {todoFilter: 'All'};
+    }
+
+    todoListFilter = (param) => {
+        this.setState({todoFilter:param});
+        const activeBtn = document.getElementById('filterBtn' + param);
+
+        document.getElementById('filterBtnAll').classList.remove('active');
+        document.getElementById('filterBtnActive').classList.remove('active');
+        document.getElementById('filterBtnCompleted').classList.remove('active');
+
+        activeBtn.classList.add('active');
     }
 
     doneTask = (e) => {
@@ -21,24 +33,41 @@ export class Content extends React.Component {
     render(){
             let items_sum = 0;
             const items = this.props.tasks.map((element, i) => {
-                if(element.status === "passive" ){
-                    items_sum++;
+
+                if(
+                    this.state.todoFilter ==='All' || 
+                    (this.state.todoFilter === 'Active' && element.status === 'passive') ||
+                    (this.state.todoFilter === 'Completed' && element.status === 'Completed')
+                ){
+                    if(element.status === "passive" ){
+                        items_sum++;
+                    }
+                    let task_id = 'task_'+ i;
+                    return(
+                        <li key={ i } id= { task_id }>
+                            <span className="task" className={ element.status }>{ i + 1 } { element.text }</span>
+                            <span className="isCompleted" onClick={ this.doneTask }></span>
+                            <span className="remove" onClick={ this.removeTask }>X</span>
+                        </li>
+                    );    
                 }
-                let task_id = 'task_'+ i;
-                return(
-                <li key={ i } id= { task_id }>
-                    <span className="task" className={ element.status }>{ i + 1 } { element.text }</span>
-                    <span className="isCompleted" onClick={ this.doneTask }></span>
-                    <span className="remove" onClick={ this.removeTask }>X</span>
-                </li>
-            );
+                
         });
          
         return(
-            <div className="content">
-                <ul type="none">
-                    { items }
-                </ul>
+            <div>
+                <div className="content">
+                    <ul type="none">
+                        { items }
+                    </ul>
+                </div>
+
+                <div className="footer">
+                    <div className="sum">0 items</div>
+                    <li className="all active" id="filterBtnAll" onClick={() => this.todoListFilter('All')}><span>All</span></li>
+                    <li className="actives" id="filterBtnActive" onClick={() => this.todoListFilter('Active')}><span>Active</span></li>
+                    <li className="completed" id="filterBtnCompleted" onClick={() => this.todoListFilter('Completed')}><span>Completed</span></li>
+                </div>
             </div>
         );
     }
